663ae3679b91ab6bb15dd57a93a37ea2f6b9830e
Vivian SARAZIN <9358572+vvnsrzn@users.noreply.github.com>
2024-10-30 15:11:43 +0100

    test: adding an iframe test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,14 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+test('index page has expected h1', async ({ page }) => {
+	await expect(page.getByRole('heading', { name: 'svelte-youtube-lite demo' })).toBeVisible();
+})
+
+test('once the button is clicked, it should display an iframe', async ({ page }) => {
+	await page.getByRole('button', { name: 'Play video' }).first().click()
+	expect(page.frameLocator('internal:role=link[name="YouTube video"i]'))
+})
